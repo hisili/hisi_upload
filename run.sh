28f4a3f9ad2c0fe46175d895a427f6c27fd13305
Jeremy Tan <jtanx@users.noreply.github.com>
2019-11-15 20:52:11 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ev
 
-REPO="https://${BOTTOKEN}@github.com/hisili/E5770s.git"
-ASSETS="https://uploads.github.com/repos/hisili/E5770s/releases"
-RECREAT="https://api.github.com/repos/hisili/E5770s/releases"
-RELEASE="https://api.github.com/repos/hisili/E5770s/releases/latest"
-DL="http://download-c1.huawei.com/download/downloadCenter?downloadId=98690&version=416242&siteCode=worldwide"
+REPO="https://${BOTTOKEN}@github.com/hisili/P8702N.git"
+ASSETS="https://uploads.github.com/repos/hisili/P8702N/releases"
+RECREAT="https://api.github.com/repos/hisili/P8702N/releases"
+RELEASE="https://api.github.com/repos/hisili/P8702N/releases/latest"
+DL="https://doc-08-2o-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/pbik00kvl8f2rjdobu8vuaqhfvevtf22/1573804800000/16243464684326191822/*/0B-U-Krbg5qbTZG43M0J3Q3JJOEE?e=download"
 
 upload_file() {
     curl --fail -H "Authorization: token ${BOTTOKEN}" -H "Content-Type: $(file -b --mime-type $1)" --data-binary @$1 "${ASSETS}/${2}/assets?name=$(basename $1)"
@@ -16,20 +16,11 @@ curl -u jtanx:${BOTTOKEN} --fail https://api.github.com/user >/dev/null
 git config --global user.email "hisi-bot@hisili"
 git config --global user.name "bot"
 
-curl -Lo unrar "https://github.com/hisili/hisi_upload/releases/download/v0/unrar-nonfree" && chmod +x unrar
+tar axf *.gz
 
-curl -LOJ "$DL"
+ls
+exit
 
-./unrar x -id *.rar >/dev/null
-
-sudo chmod 777 E5770s-923_open_src
-pushd E5770s-923_open_src
-ls -lah *
-
-sudo sh -c 'ls -l v7r11_e5770s-923_kernel_opensrc.tar.gz'
-ls v7r11_e5770s-923_kernel_opensrc.tar.gz
-tar axf v7r11_e5770s-923_kernel_opensrc.tar.gz
-pushd kernel
 git init
 git add .
 git commit -m "init" --quiet
@@ -38,9 +29,6 @@ git remote add origin "$REPO" > /dev/null 2>&1
 git push --tags --quiet --set-upstream origin master 
 popd
 
-
-rm v7r11_e5770s-923_kernel_opensrc.tar.gz
-rm *.pdf
 
 exit
 
