29a998c465ceba24d91ed86550426ff8b91deba8
Jeremy Tan <jtanx@outlook.com>
2019-11-17 14:21:52 +1100

    tt

--- run.sh
+++ run.sh
@@ -1,58 +1,44 @@
 #!/bin/bash
 set -evx
 
-REPO="https://${BOTTOKEN}@github.com/hisili/P8702N.git"
-ASSETS="https://uploads.github.com/repos/hisili/P8702N/releases"
-RECREAT="https://api.github.com/repos/hisili/P8702N/releases"
-RELEASE="https://api.github.com/repos/hisili/P8702N/releases/latest"
-DL="https://doc-08-2o-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/v9p7e08peorvu9be7m3qboclm9tg945v/1573848000000/16243464684326191822/*/0B-U-Krbg5qbTZG43M0J3Q3JJOEE?e=download"
+REPO="https://${BOTTOKEN}@github.com/hisili/hisi_upload.git"
+ASSETS="https://uploads.github.com/repos/hisili/hisi_upload/releases"
+RECREAT="https://api.github.com/repos/hisili/hisi_upload/releases"
+RELEASE="https://api.github.com/repos/hisili/hisi_upload/releases/latest"
 
 upload_file() {
     curl --fail -H "Authorization: token ${BOTTOKEN}" -H "Content-Type: $(file -b --mime-type $1)" --data-binary @$1 "${ASSETS}/${2}/assets?name=$(basename $1)"
 }
 
-#curl -u jtanx:${BOTTOKEN} --fail https://api.github.com/user >/dev/null
+curl -u jtanx:${BOTTOKEN} --fail https://api.github.com/user >/dev/null
 
 git config --global user.email "hisi-bot@hisili"
 git config --global user.name "bot"
 
-curl -LJO "$DL"
-tar axf *.gz
+which split || true
+which xz  || true
+which hg || true
+which jq || true
 
-ls -lR
-cat consumer_install
-./tmp.sh
-ls -l
-cd 100AAJX8
-export BASH=$(which bash)
-echo $BASH
-sudo rm /bin/sh
-sudo ln -s /bin/bash /bin/sh
-bash --version
-make PROFILE=DSL-2492GNAU-B3BC -j4
+cat /proc/cpuinfo
 
+relid=$(curl -s "$RELEASE" | python3 -c "import sys,json; print(json.load(sys.stdin)['id'])")
+echo $relid
 
-exit
+git clone --bare https://github.com/mozilla/gecko-dev
 
-mkdir tmp
-pushd tmp
+du -sh gecko-dev.git
 
-tar axf ../100AAJX8_4.16L.02A_consumer.tar.gz
+XZ_OPT="-9e --threads=4" tar cJf - gecko-dev.git | split --bytes=250MB - mozilla.tar.xz.
 
-git init
-git add .
-git commit -m "init" --quiet
-git tag -a "1.0.0" -m "1.0.0"
-git remote add origin "$REPO" > /dev/null 2>&1
-git push --tags --quiet --set-upstream origin master 
-popd
+for f in *.xz.*; do
+    upload_file $f "$relid"
+done
 
 
 exit
 
 #curl --fail -H "Authorization: token ${BOTTOKEN}" -H "Content-Type: application/json" --data '{"tag_name":"1.0.0", "name":"1.0.0", "body":"'"$DL"'}' "$RECREAT"
-
-relid=$(curl -s "$RELEASE" | python3 -c "import sys,json; print(json.load(sys.stdin)['id'])")
 
 pushd E5770s-923_open_src
 for f in *; do
